d1b16d95035f671309f401e81005486aa77f9084
Kamila Sulaimanova <kamilasulaimanova@Kamilas-MBP.lan>
2022-08-17 00:07:30 +0600

    Adaptive

--- frontend/src/Components/Employee/Employee.jsx
+++ frontend/src/Components/Employee/Employee.jsx
@@ -18,24 +18,24 @@ export default function Employee() {
     });
   }, []);
 
+  // render () {
+  //   const background_color = this.employee.background_color;
+  
+
   return (
     <div className='team' id='team'>
       <h1 className='section-title'>НАША КОМАНДА</h1>
-      {employees && employees.map(employee => (
+      {employees?.map(employee => (
         <div key={employee.id}>
           <div className='row'>
             <div className='left-person team-card'>
               <div className='front-side'>
-              if ({employee.background_color} = Blue) { 
                 <div className='blue team-card-title'>
                   <p>{employee.first_name}</p>
-                </div>} else {
-                <div className='red team-card-title'>
-                  <p>{employee.first_name}</p>
-                </div>}
+                </div>
                 <p className='team-card-description'>{employee.title}</p>
                 <div className='footer-card'>
-                  <a href="#"><AiOutlinePlus/>More info</a>
+                  <a href="#"><AiOutlinePlus />More info</a>
                 </div>
               </div>
               <div className='back-side'>
@@ -44,8 +44,8 @@ export default function Employee() {
                   <p>{employee.stack_set.skill}</p>
                 </div>
                 <div className='footer-card footer-back'>
-                  <a href={employee.facebook} target="_blank"><FaFacebookF/></a>
-                  <a href={employee.linkedin} target="_blank"><FaLinkedinIn/></a>
+                  <a href={employee.facebook} target="_blank"><FaFacebookF /></a>
+                  <a href={employee.linkedin} target="_blank"><FaLinkedinIn /></a>
                 </div>
               </div>
             </div>
--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,55 +1,47 @@
-import React, { useState } from 'react'
-import './Header.css'
-import img from '../img/logo.png'
-
-export default function Header() { 
-  const [header, setHeader] = useState(false);
-  const [active, setActive] = useState(false);
-
-  const handelmenu = (e) => {
-    e.preventDefault()
-    setActive(!active);
-  }
-  const changeHeader = () => {
-    if(window.scrollY >= 30){
-      setHeader(true)
-    }else{
-      setHeader(false)
-    }
-  }
-
-  window.addEventListener('scroll', changeHeader)
-  return (
-    <div className={header ? 'header active' : 'header'}>
-        <div>
-            <img className='logo' src={img} alt="" />
-        </div>
-        <div className='menu'>
-          <a href="#section">Главная</a>
-          <a href="#services">Услуги</a>
-          <a href="#team">О Нас</a>
-          <a href="#contacts">Контакты</a>
-        </div>
-        <div className='mobile-menu'>
-          <a href="" className="menu-btn" onClick={handelmenu}>
-            {active ? 
-            <div>
-              <a className='menu-active'>
-              <span></span>
-              </a>
-              <div className='side-bar'>
-               <div className='menu-side-bar'>
-                <a href="#section">Главная</a>
-                <a href="#services">Услуги</a>
-                <a href="#team">О Нас</a>
-                <a href="#contacts">Контакты</a>
-               </div>
-              </div>
-            </div> 
-              : 
-              <span></span>}
-          </a>
-        </div>
-    </div>
-  )
+import React, { useState } from 'react' 
+import './Header.css' 
+import img from '../img/logo.png' 
+import { FaBars } from "react-icons/fa"; 
+ 
+ 
+export default function Header() {  
+  const [header, setHeader] = useState(false); 
+  const [sidebar, setSidebar] = useState(false) 
+ 
+  const handleSidebar = () => { 
+    setSidebar((prevState) => !prevState) 
+  } 
+ 
+  const changeHeader = () => { 
+    if(window.scrollY >= 30){ 
+      setHeader(true) 
+    }else{ 
+      setHeader(false) 
+    } 
+  } 
+ 
+  window.addEventListener('scroll', changeHeader) 
+  return ( 
+    <div className={header ? 'header active' : 'header'}> 
+      <div className={sidebar ? 'backdrop backdrop-open' : 'backdrop'} onClick={handleSidebar}></div> 
+        <div> 
+            <img className='logo' src={img} alt="" /> 
+        </div> 
+        <div className='menu'> 
+            <a href="#section">Главная</a> 
+            <a href="#services">Услуги</a> 
+            <a href="#team">О Нас</a> 
+            <a href="#contacts">Контакты</a> 
+        </div> 
+        <div className='burger-menu'> 
+          <a href="#" className='burger-icon' onClick={handleSidebar}><FaBars/></a> 
+          <div className={sidebar ? 'side-bar side-bar-open' : 'side-bar'}> 
+            <a href="#section">Главная</a> 
+            <a href="#services">Услуги</a> 
+            <a href="#team">О Нас</a> 
+            <a href="#contacts">Контакты</a> 
+          </div> 
+        </div> 
+    </div> 
+  ) 
 }
